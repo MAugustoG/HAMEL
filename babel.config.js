b5856bca555129fd489bfe6550e5f89255afbd6c
Mateus Gomes <mataugustogomes@gmail.com>
2023-01-11 14:14:26 +0000

    Configured Folders' structure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@dtos": "./src/dtos",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@services": "./src/services",
+            "@contexts": "./src/contexts",
+            "@utils": "./src/shared/utils",
+            "@hooks": "./src/shared/hooks",
+            "@assets": "./src/shared/assets",
+            "@components": "./src/shared/components",
+          },
+        },
+      ],
+    ],
   };
 };
